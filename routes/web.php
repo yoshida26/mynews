347bc2c0cb0be6917155a418299f38e7cb17d01e
yoshida26 <yoshida26@cmind-co.jp>
2021-02-08 16:43:25 +0900

    ユーザー認証を実行する

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin'], function() {
+Route::group(['prefix' => 'admin' , 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::get('profile/create','Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+
